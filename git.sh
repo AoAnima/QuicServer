4d8924083e01ccf5a7ebdc2cb25fe79640a1fc40
AoAnima <alkazarao@gmail.com>
2024-04-26 18:00:01 +0300

    Автоматическая отправка

--- git.sh
+++ git.sh
@@ -5,3 +5,18 @@ if [ -n "$(git status --porcelain)" ]; then
     git commit -m "Автоматическая отправка"
     git push
 fi
+
+cd /home/exmaao@mfsk.int/Файлы/AoAnima.ru/jet
+git add -A
+if [ -n "$(git status --porcelain)" ]; then
+    git commit -m "Автоматическая отправка"
+    git push
+fi
+
+
+cd /home/exmaao@mfsk.int/Файлы/AoAnima.ru/QuicMarket/Logger
+git add -A
+if [ -n "$(git status --porcelain)" ]; then
+    git commit -m "Автоматическая отправка"
+    git push
+fi
