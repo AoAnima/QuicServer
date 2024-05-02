bd09e3971a94fcb0756e467fd645f440e53a410e
AoAnima <aoanima@yandex.ru>
2024-05-02 07:43:31 +0300

    эксперимерт

--- HTTPServerQuic/go.mod
+++ HTTPServerQuic/go.mod
@@ -16,24 +16,24 @@ require (
 	aoanima.ru/QErrors v0.0.0-00010101000000-000000000000
 	github.com/golang-jwt/jwt/v5 v5.2.0
 	github.com/google/uuid v1.5.0
-	github.com/quic-go/quic-go v0.41.0
+	github.com/quic-go/quic-go v0.42.0
 )
 
 require (
 	github.com/dgryski/go-metro v0.0.0-20211217172704-adc40b04c140 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
-	github.com/google/pprof v0.0.0-20231229205709-960ae82b1e42 // indirect
+	github.com/google/pprof v0.0.0-20240424215950-a892ee059fd6 // indirect
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/onsi/ginkgo/v2 v2.13.2 // indirect
-	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	github.com/onsi/ginkgo/v2 v2.17.1 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.uber.org/mock v0.4.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/exp v0.0.0-20240110193028-0dcbfd608b1e // indirect
-	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/tools v0.16.1 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
 )
--- RenderService/src/renderFunctions.go
+++ RenderService/src/renderFunctions.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	. "aoanima.ru/Logger"
+	jet "gitverse.ru/Ao/jet"
 )
 
 func генераторИд() string {
@@ -50,7 +51,111 @@ func Сумма(числа ...interface{}) float64 {
 	}
 	return сумма
 }
+func jetРендерФункции() jet.VarMap {
+	функции := jet.КартаФункций{
+		"Сумма": Сумма,
+		"ИД": func() string {
+			timestamp := time.Now().UnixNano() / int64(time.Millisecond)
+			return strconv.FormatInt(timestamp, 36)
+		},
+		"Слайс": func(аргументы ...interface{}) []interface{} {
+			return аргументы
+		},
+		"Мап": func(КлючЗначение ...interface{}) map[string]interface{} {
+			if len(КлючЗначение)%2 != 0 {
+				return map[string]interface{}{
+					"ошибка": "не чётное количество аргументов для map",
+				}
+			}
+			Карта := make(map[string]interface{}, len(КлючЗначение)/2)
+			for i := 0; i < len(КлючЗначение); i += 2 {
+				key, ok := КлючЗначение[i].(string)
+				if !ok {
+					return map[string]interface{}{
+						"ошибка": fmt.Sprintf("Ключ %+v должен быть строкой", КлючЗначение[i]),
+					}
+				}
+				// Инфо(" %+v %#T \n", КлючЗначение[i+1], КлючЗначение[i+1])
 
+				Карта[key] = КлючЗначение[i+1]
+			}
+			return Карта
+		},
+		"ВременнаяМетка": func() int64 {
+			return time.Now().Unix()
+		},
+		"вСтроку": func(data interface{}) string {
+			return data.(string)
+		},
+		"вЦелое": func(data float64) int {
+			return int(data)
+		},
+		"ЧислоВСтроку": func(data float64) string {
+			return strconv.Itoa(int(data))
+		},
+		"СтрокуВЧисло": func(data string) (int, error) {
+			число, err := strconv.Atoi(data)
+			if err != nil {
+				Ошибка("  %+v \n", err)
+				return число, err
+			}
+			return число, nil
+		},
+		"вJSON": func(data interface{}) interface{} {
+			//Инфо("  reflect.TypeOf %+v",  reflect.TypeOf(data).Kind())
+			//if reflect.TypeOf(data).Kind().String() == "map" {
+			if data == nil {
+				return nil
+			}
+			r, e := json.Marshal(data)
+			Инфо("вJSON data %+v r %+v", data, r)
+			if e != nil {
+				Ошибка("  %+v", e)
+			}
+			return string(r)
+			//}
+		},
+		"Пусто": func(data interface{}) interface{} {
+
+			if data == nil {
+				return "Нет данных"
+			} else {
+				switch data.(type) {
+				case int64:
+					return strconv.Itoa(int(data.(float64)))
+				case map[string]interface{}:
+					return data
+				}
+
+			}
+
+			return data
+		},
+		"TypeOf": func(n interface{}) string {
+			return reflect.TypeOf(n).String()
+		},
+		"Тип": func(n interface{}) string {
+			return reflect.TypeOf(n).String()
+		},
+		"Строка": func(Строки ...string) string {
+			Результат := ""
+			for _, Стр := range Строки {
+				Результат = Результат + Стр
+			}
+			return Результат
+		},
+	}
+
+	jetКартаФункций := make(jet.VarMap)
+	for имяФункции, телоФункции := range функции {
+		значение := reflect.ValueOf(телоФункции)
+		if значение.Kind() != reflect.Func {
+			Ошибка("значение для %+s не является функцией: %+v ", имяФункции, значение.Kind())
+		}
+		jetКартаФункций[имяФункции] = reflect.ValueOf(значение)
+	}
+	return jetКартаФункций
+}
 func РендерФункции() template.FuncMap {
 	return template.FuncMap{
 		"jsStr": func(s string) template.JSStr {
